b7259e5388877cdfde41ea7c2e44c7d248e69076
Julien Bourdeau <julien@julienbourdeau.com>
2024-02-25 11:57:57 +0100

    remove algolia gem

--- Gemfile
+++ Gemfile
@@ -50,8 +50,6 @@ gem "redcarpet"
 gem "rainbow"
 
 
-# gem "algoliasearch-rails", path: "../../../Algolia/algoliasearch-rails"
-
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,19 +1,5 @@
 class Reaction < ApplicationRecord
-  include AlgoliaSearch
-
   belongs_to :post
 
   validates :name, inclusion: {in: %w(up down star)}
-
-  algoliasearch id: :algolia_id  do
-    attributes :name, :post_id
-    searchableAttributes ['name']
-    attributesForFaceting ['name']
-  end
-
-  private
-
-  def algolia_id
-    "re_#{id}"
-  end
 end
